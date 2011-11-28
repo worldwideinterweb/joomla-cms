bdd3ff68b58f0267b6abffd1d3e1c6e0548bd784
root <root@sql1.worldwideinterweb.com>
2011-11-28 12:54:02 -0600

    $committime

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#<?php die('Forbidden.'); ?>
+#Date: 2011-11-28 18:53:03 UTC
+#Software: Joomla Platform 11.2.0 Stable+Modified [ Omar ] 27-Jul-2011 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2011-11-28	18:53:03	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2011-11-28	18:53:13	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
