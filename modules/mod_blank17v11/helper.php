7afd4ddf10890402c5697dadbaada080fd51b66a
root <root@sql1.worldwideinterweb.com>
2011-11-30 20:43:02 -0600

    $committime

---
+++ modules/mod_blank17v11/helper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ *
+ * @package   mod_blank17v11
+ * copyright Bob Galway
+ * @license GPL3
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
---
+++ modules/mod_blank17v11/mod_blank17v11.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ *
+ * @package   mod_blank16v52
+ * copyright Bob Galway
+ * @license GPL3
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+require JModuleHelper::getLayoutPath('mod_blank17v11', $params->get('layout', 'default'));
+
