ac98aaa464028ddcb323957522f82b3290c267bc
root <root@sql1.worldwideinterweb.com>
2012-03-26 23:50:45 -0500

    $committime

--- components/com_k2/templates/default/item.php
+++ components/com_k2/templates/default/item.php
@@ -203,6 +203,7 @@ defined('_JEXEC') or die('Restricted access');
 	  <?php echo $this->item->event->K2BeforeDisplayContent; ?>
 
 	  <?php if($this->item->params->get('itemImage') && !empty($this->item->image)): ?>
+<? var_dump($this->params); ?>
 	  <!-- Item Image -->
 	  <div class="itemImageBlock">
 		  <span class="itemImage">
