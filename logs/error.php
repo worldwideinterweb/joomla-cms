e1595c6942a7e3a339d532b3bb14818c9fc03452
root <root@sql1.worldwideinterweb.com>
2012-09-28 16:09:59 -0500

    $committime

--- logs/error.php
+++ logs/error.php
@@ -141,3 +141,4 @@
 2012-09-24	14:24:40	INFO	46.160.85.231	Joomla FAILURE: 	Invalid password
 2012-09-26	21:52:21	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
 2012-09-27	21:21:56	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
+2012-09-28	21:03:06	INFO	74.212.251.170	Joomla FAILURE: 	Invalid password
